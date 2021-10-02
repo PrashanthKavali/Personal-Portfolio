7abc1de17317662ba6a8bf99592edd91ae5099af
Prashanth <84450822+prashanthkavali@users.noreply.github.com>
2021-10-02 19:33:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -155,6 +155,60 @@
       }
     }
   });
+  
+    /**
+   * Type writer effect
+   */
+  const TypeWriter = function (txtElement, words, wait = 1000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  //Type Method
+  TypeWriter.prototype.type = function () {
+    const current = this.wordIndex % this.words.length;
+    const fulltxt = this.words[current];
+    if (this.isDeleting) {
+      this.txt = fulltxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fulltxt.substring(0, this.txt.length + 1);
+    }
+
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`
+
+    //Type Speed
+    let typeSpeed=200;
+    if(this.isDeleting){
+      typeSpeed/=2;
+    }
+
+    if(!this.isDeleting&&this.txt===fulltxt){
+      typeSpeed=this.wait;
+      this.isDeleting=true;
+    }
+    else if(this.isDeleting&&this.txt===''){
+      this.isDeleting=false;
+      this.wordIndex++;
+      typeSpeed=300;
+    }
+
+    setTimeout(() => this.type(), typeSpeed)
+  }
+
+  document.addEventListener('DOMContentLoaded', init);
+
+  function init() {
+    const txtElement = document.querySelector('.type-text');
+    const words = JSON.parse(txtElement.getAttribute('data-words'));
+    const wait = txtElement.getAttribute('data-wait');
+    //Initialise Type Writer
+    new TypeWriter(txtElement, words, wait);
+  }
 
   /**
    * Porfolio isotope and filter
